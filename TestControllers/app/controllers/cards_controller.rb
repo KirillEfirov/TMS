534931174cce6773b12c683cdbfc52d2f8d37b96
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-05-01 23:45:01 +0300

    Change the redirection path

--- TestControllers/app/controllers/cards_controller.rb
+++ TestControllers/app/controllers/cards_controller.rb
@@ -1,25 +1,25 @@
 class CardsController < ApplicationController
   def create
     @folder = Folder.find(params[:folder_id])
-    @card = Card.new(word: params[:card][:word], translation: params[:card][:translation], folder: @folder)
+    @card = Card.new(card_params.merge({ folder: @folder }))
 
-    @card.save ? flash[:success] = 'Card created successfully' : flash[:error] = 'Card creation failed'
+    @card.save ? flash[:success] = 'Card was created successfully' : flash[:error] = 'Card creation failed'
 
-    redirect_to folder_path(@folder)
+    redirect_to folder_cards_path(@folder)
   end
 
   def update
     @folder = Folder.find(params[:folder_id])
     @card = @folder.cards.find(params[:id])
 
-    redirect_to folder_path(@folder) if @card.update(card_params)
+    redirect_to folder_cards_path(@folder) if @card.update(card_params)
   end
 
   def destroy
     @folder = Folder.find(params[:folder_id])
     @card = Card.find(params[:id])
 
-    redirect_to folder_path(@folder) if @card.destroy
+    redirect_to folder_cards_path(@folder) if @card.destroy
   end
 
   private
