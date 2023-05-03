07e3b524182d7ebced515f2ebf473d80d9c0dc95
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-05-03 11:43:56 +0300

    Create new path to learn random words from different folders

--- TestControllers/config/routes.rb
+++ TestControllers/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'folders#index'
 
+  get '/folders/learn', to: 'folders#learn'
+
   resources :folders, except: %i[new edit] do
     resources :cards, only: %i[create update destroy]
   end
