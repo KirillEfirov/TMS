3bd7e17e29d1c29ab2be817b93ad4ddb2042ebb3
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-05-07 16:39:06 +0300

    Add registration/authorization paths

--- TestControllers/config/routes.rb
+++ TestControllers/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, except: %i[new edit]
+
+  get '/signup', to: 'users#new'
+  get '/login', to: 'authentication#new'
+  post '/login', to: 'authentication#login'
+  delete '/logout', to: 'authentication#destroy'
+
   root 'folders#index'
 
   get '/folders/learn', to: 'folders#learn'
