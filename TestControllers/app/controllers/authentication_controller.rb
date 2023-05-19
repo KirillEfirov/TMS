4aa18f4f02ce5b2fdc2405f08e79680f3a9e92f5
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-05-19 21:10:06 +0300

    Authenticate the user or ask for loginning one more time

---
+++ TestControllers/app/controllers/authentication_controller.rb
@@ -0,0 +1,27 @@
+class AuthenticationController < ApplicationController
+  # skip_before_action :authenticate_request
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+
+    if @user&.authenticate(params[:user][:password])
+      token = encode_token(user_id: @user.id)
+      log_in(token)
+
+      redirect_to user_path(@user)
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unauthorized
+    end
+  end
+
+  def destroy
+    log_out
+
+    redirect_to root_path
+  end
+end
