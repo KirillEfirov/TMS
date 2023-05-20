6f02cfb4de27ce50a4edf6fdc5d19751e8671060
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-05-20 20:24:18 +0300

    Add user reference to folder

--- TestControllers/db/schema.rb
+++ TestControllers/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_143724) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_163547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_143724) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_folders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_143724) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "folders", "users"
 end
