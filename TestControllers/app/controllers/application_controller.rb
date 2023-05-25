40cd14bf37663d56c78a68d1c56ff902e4f9e8f1
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-05-25 11:24:49 +0300

    Move log_in/log_out methods to Application helper to hold authentication logic in one place

--- TestControllers/app/controllers/application_controller.rb
+++ TestControllers/app/controllers/application_controller.rb
@@ -1,23 +1,3 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
-
-  def log_in(token)
-    session[:user_token] = token
-  end
-
-  def log_out
-    session.delete(:user_token)
-    @current_user = nil
-  end
-
-=begin
-  private
-
-  def authenticate_request
-    header = request.headers['Authorization']
-    header = header.split(' ').last if header
-    decoded = decode_token(header)
-    @current_user = User.find(decoded[:user_id])
-  end
-=end
 end
