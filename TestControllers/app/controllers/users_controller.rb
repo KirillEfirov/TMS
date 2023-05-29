44bc28aec732f96a2c3bda660f42f55437fa0d77
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-05-29 10:33:10 +0300

    Create action to upload an avatar

--- TestControllers/app/controllers/users_controller.rb
+++ TestControllers/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: %i[show update destroy]
+  before_action :find_user, only: %i[show upload destroy]
 
   def index
     @users = User.all
@@ -24,17 +24,52 @@ class UsersController < ApplicationController
     end
   end
 
-  def update
-    unless @user.update(user_params)
-      render json: { errors: @user.errors.full_messages },
-             status: 503
+=begin
+  def upload
+    if params[:user].present? && params[:user][:avatar].present?
+      @user.avatar.attach(params[:user][:avatar])
+      # user.update(avatar: params[:user][:avatar])
+      redirect_to user, notice: 'Avatar uploaded successfully.'
+    else
+      redirect_to user, notice: 'Avatar uploaded unsuccessfully.'
+    end
+  end
+=end
+
+  def upload
+    user = User.find(params[:id])
+    if params[:user].present? && params[:user][:avatar].present?
+      uploaded_file = params[:user][:avatar]
+      file_path = Rails.root.join('storage', uploaded_file.original_filename)
+
+      # Save the file to the desired storage location
+      File.open(file_path, 'wb') do |file|
+        file.write(uploaded_file.read)
+      end
+
+      # Update the user's avatar attribute with the file path
+      # user.update(avatar: file_path.to_s)
+
+      # user.avatar.attach(params[:user][:avatar])
+      # user.update
+
+      # user.avatar = file_path.to_s
+      # user.save
+
+      user.avatar.attach(io: File.open(file_path.to_s), filename: uploaded_file.original_filename)
+
+      # user.avatar.attach(io: File.open(file_path), filename: uploaded_file.original_filename)
+
+      redirect_to user, notice: 'Avatar uploaded successfully!'
+    else
+      redirect_to user, alert: 'No file selected for upload!'
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
   end
 
   def find_user
