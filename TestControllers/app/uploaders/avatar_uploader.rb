57626ff956e4f8d753ce1f3d3999a5daa5edd611
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-05-30 12:39:15 +0300

    Setting size of the image

--- TestControllers/app/uploaders/avatar_uploader.rb
+++ TestControllers/app/uploaders/avatar_uploader.rb
@@ -1,7 +1,9 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+
+  process resize_to_fill: [350, 200]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -30,7 +32,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+  #   process resize_to_fit: [250, 250]
   # end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
