ae4d3521dda21b2e07add527c3090afd64efc11b
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-06-05 18:56:44 +0300

    Add helper to display image in the welcome email

--- TestControllers/app/mailers/application_mailer.rb
+++ TestControllers/app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  helper(EmailHelper)
+
+  default from: 'from@example.com'
+  layout 'mailer'
 end
