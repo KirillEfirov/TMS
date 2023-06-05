1f999786074da49fd623c8fc136e80d8771155dc
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-06-05 16:19:07 +0300

    Add a mailer action that sends users a signup email

--- TestControllers/app/mailers/user_mailer.rb
+++ TestControllers/app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  def send_signup_email(user)
+    @user = user
+
+    mail(:to => @user.email,
+         :subject => 'Thanks for signing up for our amazing app')
+  end
 end
